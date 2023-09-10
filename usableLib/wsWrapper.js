b537dfdae278ff710d1071acc2df3b47b1436026
Anton <dev@atjn.dk>
2023-09-10 15:51:44 +0200

    Make ws wrapper more robust

--- usableLib/wsWrapper.js
+++ usableLib/wsWrapper.js
@@ -1,17 +1,20 @@
 /**
  * @file
- * When upgrading from CJS to ESM, the `ws` package changes its exports.
+ * The `ws` package changes its exports based on which module system it is being imported by.
+ * This is very buggy because different bundlers and systems will activate different exports, and thus make it impossible for code to know which export layout to use.
  * This wrapper restores the old export structure to make it compatible with the Gun code, which is expecting the CJS export layout.
  */
 
-import { createWebSocketStream, Receiver, Sender, WebSocket, WebSocketServer } from "ws";
+const _ws = await import("ws");
 
-WebSocket.createWebSocketStream = createWebSocketStream;
-WebSocket.Server = WebSocketServer;
-WebSocket.Sender = Sender;
-WebSocket.Receiver = Receiver;
-WebSocket.WebSocket = WebSocket;
-WebSocket.WebSocketServer = WebSocketServer;
+const _WebSocket = _ws.WebSocket ?? _ws.default;
 
-export default WebSocket;
+_WebSocket.createWebSocketStream ??= _ws.createWebSocketStream;
+_WebSocket.Server ??= _ws.WebSocketServer;
+_WebSocket.Sender ??= _ws.Sender;
+_WebSocket.Receiver ??= _ws.Receiver;
+_WebSocket.WebSocket ??= _ws.WebSocket;
+_WebSocket.WebSocketServer ??= _ws.WebSocketServer;
+
+export default _WebSocket;
 
